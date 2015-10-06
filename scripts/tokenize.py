97ebf68b4de691a060447432d3f6b3ef762346b4
Robin Ricard <ricard.robin@gmail.com>
2015-10-06 21:36:15 +0200

    First bad tokenization

--- scripts/tokenize.py
+++ scripts/tokenize.py
@@ -1,17 +1,35 @@
 from subprocess import Popen, PIPE, STDOUT
 import sys
+import re
 from os import listdir
 from os.path import isfile, join
 import fnmatch
 
 def load_z_file(file_path):
-    cmd = 'gzip -d -c %s' % (file_path)
-    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    return p.stdout.read()
+    return Popen(
+        'gzip -d -c %s' % (file_path),
+        shell=True,
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=STDOUT,
+        close_fds=True
+    ).stdout.read()
 
-def load_corpus(corpus_path):
-    file_names = listdir(corpus_path)
-    return map(lambda n: load_z_file("%s/%s" % (corpus_path, n)),
-        filter(lambda n: fnmatch.fnmatch(n, '*.z'), file_names))
+def tokenize_text(text):
+    return " ".join(
+        map(
+            lambda s: s.lower(),
+            re.compile(r"(\s|\n)+").split(text)))
 
-print(load_corpus("./corpus"))
+def tokenize_corpus(corpus_path):
+    return "\n".join(
+        map(
+            lambda n: " ".join(
+                tokenize_text(
+                    load_z_file(
+                        "%s/%s" % (corpus_path, n)))),
+            filter(
+                lambda n: fnmatch.fnmatch(n, '*.z'),
+                listdir(corpus_path))))
+
+print(tokenize_corpus("./corpus"))
