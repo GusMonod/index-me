7af5ff633883ba81da8d246ff011c76dab118fd6
LilCartell <lilian.cartellier01@orange.fr>
2015-11-13 15:34:26 +0100

    Added entries election function for merger

--- merger/main.c
+++ merger/main.c
@@ -8,6 +8,34 @@
 #include "util/parser.h"
 #include "util/types.h"
 
+/*Returns an array of int containing the indexes of the entries to be elected
+from termEntries, an writes the size of the array in outElectedEntriesSize*/
+int* electedEntries (TermEntry* termEntries, int termEntriesSize,
+    int* outElectedEntriesSize) {
+  wchar_t* electedWord = NULL;
+  int currentElectedEntriesSize = 0;
+  int* electedEntries;
+
+  for (int i = 0; i < termEntriesSize; i++) {
+    if (electedWord == NULL ||
+        wcscmp(electedWord, termEntries[i].token) > 0) {
+      electedWord = termEntries[i].token;
+      currentElectedEntriesSize = 1;
+    } else if (wcscmp(electedWord, termEntries[i].token) == 0) {
+      currentElectedEntriesSize++;
+    }
+  }
+
+  *outElectedEntriesSize = currentElectedEntriesSize;
+  electedEntries = (int*) pMalloc(currentElectedEntriesSize * sizeof(int));
+  int currentElectedIndex = 0;
+  for (int i = 0; i < termEntriesSize; i++) {
+    if (wcscmp(electedWord, termEntries[i].token) == 0) {
+      electedEntries[currentElectedIndex++] = i;
+    }
+  }
+}
+
 int main() {
   setlocale(LC_ALL, "en_US.UTF-8");  // For unicode handling
 
